cf9732ca50ebb03bf793788181bcc05f4f427800
Haardhik <kunderhaardhik@gmail.com>
2023-09-05 00:57:34 +0530

    slide added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
-
+import Tilt from "react-parallax-tilt";
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
@@ -72,62 +72,52 @@ const Contact = () => {
     <div
       className={`xl:mt-10 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
     >
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.80]  p-8 rounded-2xl'
-      >
-        <p className="sm:text-[18px] text-[14px] text-[#ede1be] uppercase tracking-wider">Get in touch</p>
-        <h3 className="text-[#ede1be] font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Contact.</h3>
+      <motion.div >
+  <Tilt
+    options={{
+      max: 45,
+      scale: 1,
+      speed: 450,
+    }}
+    className='bg-[#111111] p-5 rounded-2xl sm:w-[560px] w-full'
+  >
+    <div className='relative w-full h-[530px]'>
+      {/* <img
+        src={image}
+        alt='project_image'
+        className='w-full h-full object-cover rounded-2xl'
+      /> */}
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='mt-10 flex flex-col gap-8'
+      <div className='absolute inset-0 flex justify-end m-3 card-img_hover gap-2'>
+        <div
+          // onClick={() => window.open(source_code_link, "_blank")}
+          className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
         >
-          <label className='flex flex-col'>
-            <span className='text-[#ede1be] font-medium mb-4'>Your Name</span>
-            <input
-              type='text'
-              name='name'
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className='bg-[#222222] py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-[#ede1be] font-medium mb-4'>Your email</span>
-            <input
-              required
-              type='email'
-              name='email'
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className='bg-[#222222] py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-[#ede1be] font-medium mb-4'>Your Message</span>
-            <textarea
-              rows={6}
-              required
-              name='message'
-              value={form.message}
-              onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-[#222222] py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
+          {/* <img
+            src={reactjs}
+            alt='source code'
+            className='w-1/2 h-1/2 object-contain'
+          /> */}
+        </div>
+        
+      </div>
+    </div>
 
-          <button
-            type='submit'
-            className='bg-[#111111] py-3 px-8 rounded-xl outline-none w-fit text-[#ede1be] font-bold shadow-md shadow-primary'
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
-      </motion.div>
+    <div className='mt-5'>
+      <h3 className='text-[#ede1be] font-bold text-[24px]'>husndc</h3>
+      <p className='mt-2 text-secondary text-[14px]'>sdcds kcjdc</p>
+    </div>
+
+    <div className='mt-4 flex flex-wrap gap-2'>
+      
+        <p className={`text-[14px] `}
+        >
+          sadcslc,d;slc,
+        </p>
+     
+    </div>
+  </Tilt>
+</motion.div>
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
